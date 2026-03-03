8bf6cce884075f768553268bdf7c4a17c256b17f
entity01business-svg <entity01business@gmail.com>
2026-03-03 20:05:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get("/download-work-order", (req, res) => {
     const filePath = path.join(__dirname, "work-order.pdf");
 
     res.setHeader("Content-Type", "application/pdf");
-    res.setHeader("Content-Disposition", "inline");
+    res.setHeader("Content-Disposition", "attachment; filename=work-order.pdf");
 
     res.sendFile(filePath);
 });
@@ -16,4 +16,5 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
+
 });

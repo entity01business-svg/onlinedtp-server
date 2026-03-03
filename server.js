a46bfb731b672ae4acfd47c9bbebcd254dc52da7
entity01business-svg <entity01business@gmail.com>
2026-03-03 19:49:21 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/download-work-order", (req, res) => {
+    const filePath = path.join(__dirname, "work-order.pdf");
+
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", "inline");
+
+    res.sendFile(filePath);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});

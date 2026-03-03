bacb89c0e67c15e735e4fedd19d43fcf6d4f59f1
entity01business-svg <entity01business@gmail.com>
2026-03-03 20:14:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,47 @@ const path = require("path");
 const app = express();
 
 app.get("/download-work-order", (req, res) => {
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Work Order</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            html, body {
+                margin:0;
+                padding:0;
+                height:100%;
+                background:#fff;
+            }
+            iframe {
+                position:absolute;
+                width:100%;
+                height:100%;
+                border:none;
+            }
+        </style>
+    </head>
+    <body>
+        <iframe src="/pdf"></iframe>
+
+        <script>
+            window.onload = function() {
+                setTimeout(() => {
+                    window.print();
+                }, 800);
+            };
+        </script>
+    </body>
+    </html>
+    `);
+});
+
+app.get("/pdf", (req, res) => {
     const filePath = path.join(__dirname, "work-order.pdf");
 
     res.setHeader("Content-Type", "application/pdf");
-    res.setHeader("Content-Disposition", "attachment; filename=work-order.pdf");
-
+    res.setHeader("Content-Disposition", "inline");
     res.sendFile(filePath);
 });
 
@@ -16,5 +52,4 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
-
 });
